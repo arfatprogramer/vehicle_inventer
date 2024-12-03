ef116a85cc62322aa587bb34f4d7187476275b93
arfatprogramer <arfata701@gmail.com>
2024-12-03 18:24:40 +0530

    setup done

---
+++ admin/index.php
@@ -0,0 +1,8 @@
+<?php include"./layout/headder.php"?>
+
+<div class="flex justify-center items-center h-screen border">
+    <div class="flex justify-center items-center border-2 border-black p-3">Box</div>
+    <i class="fi fi-rr-user"></i>
+</div>
+
+<?php include"./layout/footer.php"?>
---
+++ admin/layout/headder.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="./output.css" rel="stylesheet"> <!-- tailwind css -->
+  <link href="./layout/css/uicons-regular-rounded.css" rel="stylesheet"> <!-- Icons css -->
+  
+</head>
+<body>
+  
+  <!-- npx tailwindcss -i ./admin/input.css -o ./admin/output.css --watch -->
